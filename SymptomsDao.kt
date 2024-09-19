685bbd6a02362bfe5673d4f7d44de78adf1201a8
Satwik-Ponnam <satwikponnam@gmail.com>
2024-09-18 23:28:44 -0700

    Add files via upload

---
+++ SymptomsDao.kt
@@ -0,0 +1,26 @@
+package com.example.diashield
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SymptomsDao {
+
+    @Upsert
+    suspend fun upsertHealthInfo(healthInformation: Symptoms)
+
+    @Delete
+    suspend fun delete(healthInformation: Symptoms)
+
+    @Query("SELECT * FROM symptoms")
+    fun getAllHealthInformation() : Flow<List<Symptoms>>
+
+    @Query("DELETE FROM symptoms")
+    suspend fun deleteAll()
+
+    @Query("SELECT * FROM symptoms ORDER BY id DESC LIMIT 1")
+    abstract fun getLatestHealthInfo(): Symptoms?
+}
